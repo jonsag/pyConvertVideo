7c7e335d60c6b2fda5d254aafa787f2bb31d03c7
jon <jon@HP-Pavilion-15>
2019-02-04 19:28:01 +0100

    update to python3

--- error.py
+++ error.py
@@ -5,33 +5,33 @@
 import sys
 
 def onError(errorCode, extra):
-    print "\nError:"
+    print("\nError:")
     if errorCode in(1, 2):
-        print extra
+        print(extra)
         usage(errorCode)
     elif errorCode in (4, 5, 6 ,7 ,8, 11, 12):
-        print extra
+        print(extra)
         sys.exit(errorCode)
     elif errorCode in (9, 10, 13, 14):
-        print extra
-        raw_input('Press [Return] key to continue')
+        print(extra)
+        input('Press [Return] key to continue')
 
 def usage(exitCode):
-    print "\nUsage:"
-    print "----------------------------------------"
-    print "%s -f <in file> [-o <out directory>] [-i -k -r -v]" % sys.argv[0]
-    print "  Creates a single contact sheet for <in file> [and puts it in <out directory> ]"
-    print 
-    print "%s -p <in directory> [-o <out directory>][-i -k -r -v]" % sys.argv[0]
-    print "  Scans <in directory> for video files, creates contact sheets from them [and puts them in <outdirectory>]"
-    print
-    print "    Otions:"
-    print "    -i display file 'i'nfo"
-    print "    -k 'k'eep going on errors"
-    print "    -r 'r'ename files to safe file names"
-    print "    -v 'v'erbose output"
-    print
-    print "%s -h" % sys.argv[0]
-    print "  Prints this"
+    print("\nUsage:")
+    print("----------------------------------------")
+    print("%s -f <in file> [-o <out directory>] [-i -k -r -v]" % sys.argv[0])
+    print("  Creates a single contact sheet for <in file> [and puts it in <out directory> ]")
+    print() 
+    print("%s -p <in directory> [-o <out directory>][-i -k -r -v]" % sys.argv[0])
+    print("  Scans <in directory> for video files, creates contact sheets from them [and puts them in <outdirectory>]")
+    print()
+    print("    Otions:")
+    print("    -i display file 'i'nfo")
+    print("    -k 'k'eep going on errors")
+    print("    -r 'r'ename files to safe file names")
+    print("    -v 'v'erbose output")
+    print()
+    print("%s -h" % sys.argv[0])
+    print("  Prints this")
     
     sys.exit(exitCode)
--- misc.py
+++ misc.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # Encoding: UTF-8
 
-import os, ConfigParser
+import os, configparser
 
-config = ConfigParser.ConfigParser()  # define config file
+config = configparser.ConfigParser()  # define config file
 config.read("%s/config.ini" % os.path.dirname(os.path.realpath(__file__)))  # read config file
 
 videoTypes = (config.get('video', 'videoTypes')).split(',')  # allowed file types
